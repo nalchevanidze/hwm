4e1cbc7422a90784d3669f07d4dc39fd820f242f
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-02-18 22:52:23 +0100

    set-default

--- hwm/src/HWM/CLI/Command/Environment/SetDefault.hs
+++ hwm/src/HWM/CLI/Command/Environment/SetDefault.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module HWM.CLI.Command.Environment.SetDefault (EnvSetDefaultOptions, runEnvSetDefault) where
 
+import HWM.Core.Common (Name)
 import HWM.Core.Parsing (ParseCLI (..))
-import Options.Applicative (pure)
-import HWM.Domain.ConfigT (ConfigT)
+import HWM.Domain.Config (Config (..))
+import HWM.Domain.ConfigT (ConfigT, updateConfig)
+import qualified HWM.Domain.Matrix as Matrix
+import Options.Applicative (help, metavar, strArgument)
 import Relude
+import Control.Monad.Error.Class (throwError)
 
-data EnvSetDefaultOptions = EnvSetDefaultOptions
+newtype EnvSetDefaultOptions = EnvSetDefaultOptions {envName :: Text}
   deriving (Show)
 
 instance ParseCLI EnvSetDefaultOptions where
-  parseCLI = pure EnvSetDefaultOptions
-
+  parseCLI = EnvSetDefaultOptions <$> strArgument (metavar "ENV" <> help "Name of the environment to set as default")
 
 runEnvSetDefault :: EnvSetDefaultOptions -> ConfigT ()
-runEnvSetDefault _ = pure ()
+runEnvSetDefault EnvSetDefaultOptions {..} = do
+  -- Validate environment exists using Matrix.getBuildEnvironment
+  _ <- Matrix.getBuildEnvironment (Just envName)
+  let setDefaultEnv cfg = cfg { matrix = (matrix cfg) { Matrix.defaultEnvironment = envName } }
+  updateConfig (pure . setDefaultEnv) $ do
+    putText $ "Default environment set to: " <> envName <> "\n"
