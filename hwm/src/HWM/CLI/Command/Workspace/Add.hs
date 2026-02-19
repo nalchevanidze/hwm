774127d34e463b8767755d1017cfb9c74f7e7af1
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-02-19 20:12:37 +0100

    add workspace groups

--- hwm/src/HWM/CLI/Command/Workspace/Add.hs
+++ hwm/src/HWM/CLI/Command/Workspace/Add.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module HWM.CLI.Command.Workspace.Add (WorkspaceAddOptions, runWorkspaceAdd) where
 
 import HWM.Core.Common (Name)
 import HWM.Core.Parsing (ParseCLI (..))
-import HWM.Domain.ConfigT (ConfigT)
-import HWM.Domain.Workspace (parseWorkspaceId)
+import HWM.Domain.Config (Config (..))
+import HWM.Domain.ConfigT (ConfigT, updateConfig)
+import HWM.Domain.Workspace (WorkspaceGroup (..), askWorkspaceGroups, parseWorkspaceId, selectGroup)
+import HWM.Runtime.UI (putLine)
 import Options.Applicative (help, metavar, strArgument)
 import Relude
-import HWM.Runtime.UI (putLine)
 
 newtype WorkspaceAddOptions = WorkspaceAddOptions {workspaceId :: (Name, Maybe Name)}
   deriving (Show)
@@ -18,11 +19,12 @@ instance ParseCLI WorkspaceAddOptions where
   parseCLI = WorkspaceAddOptions . parseWorkspaceId <$> strArgument (metavar "NAME" <> help "Name of the workspace to add")
 
 runWorkspaceAdd :: WorkspaceAddOptions -> ConfigT ()
-runWorkspaceAdd (WorkspaceAddOptions (groupId, Nothing)) = do 
-   -- TODO: implement workspace addition
-   putLine $ "Adding workspace group: " <> groupId 
-   pure ()
-runWorkspaceAdd (WorkspaceAddOptions (groupId, Just memberId)) = do 
-   -- TODO: implement workspace addition
-   putLine $ "Adding package: " <> memberId <> " to workspace group: " <> groupId
-   pure ()
+runWorkspaceAdd (WorkspaceAddOptions (groupId, Nothing)) = do
+  -- TODO: implement group addition
+  putLine $ "Adding workspace group: " <> groupId
+  updateConfig (\cfg -> pure $ cfg {workspace = workspace cfg ++ [WorkspaceGroup groupId Nothing [] Nothing Nothing]}) $ pure ()
+runWorkspaceAdd (WorkspaceAddOptions (groupId, Just memberId)) = do
+  g <- selectGroup groupId =<< askWorkspaceGroups
+  -- TODO: implement package addition
+  putLine $ "Adding package: " <> memberId <> " to workspace group: " <> show g
+  pure ()
