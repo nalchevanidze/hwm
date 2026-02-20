b68acecafe2b254caf124caffbe10c0a5fc2407f
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-02-20 13:03:16 +0100

    ParseCLI

--- hwm/src/HWM/CLI/Command/Release/Package.hs
+++ hwm/src/HWM/CLI/Command/Release/Package.hs
@@ -1,23 +1,26 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
-module HWM.CLI.Command.Release.Package (ReleasePackageOptions(..), parseCLI, runReleasePackage) where
+module HWM.CLI.Command.Release.Package (ReleasePackageOptions (..), parseCLI, runReleasePackage) where
 
-import Relude
-import Options.Applicative (Parser, strOption, long, metavar, help, optional)
 import HWM.Core.Common (Name)
+import HWM.Core.Parsing (ParseCLI (..))
 import HWM.Domain.ConfigT (ConfigT)
+import Options.Applicative (help, long, metavar, strOption)
+import Relude
 
 -- | Options for 'hwm release package'
 data ReleasePackageOptions = ReleasePackageOptions
-  { packageName :: Name
-  , outFile :: Maybe FilePath
-  } deriving (Show)
+  { packageName :: Name,
+    outFile :: Maybe FilePath
+  }
+  deriving (Show)
 
-parseCLI :: Parser ReleasePackageOptions
-parseCLI = ReleasePackageOptions
-  <$> strOption (long "package" <> metavar "PACKAGE" <> help "Name of the package to release")
-  <*> optional (strOption (long "out" <> metavar "FILE" <> help "Export resulting file paths to FILE"))
+instance ParseCLI ReleasePackageOptions where
+  parseCLI =
+    ReleasePackageOptions
+      <$> strOption (long "package" <> metavar "PACKAGE" <> help "Name of the package to release")
+      <*> optional (strOption (long "out" <> metavar "FILE" <> help "Export resulting file paths to FILE"))
 
 runReleasePackage :: ReleasePackageOptions -> ConfigT ()
 runReleasePackage _opts = pure () -- TODO: implement native build, archive, hash, and export logic
--- hwm/src/HWM/CLI/Command/Release/Root.hs
+++ hwm/src/HWM/CLI/Command/Release/Root.hs
@@ -3,18 +3,21 @@
 
 module HWM.CLI.Command.Release.Root (ReleaseCommand (..), runRelease) where
 
+import HWM.CLI.Command.Release.Package (ReleasePackageOptions, runReleasePackage)
 import HWM.Core.Parsing (ParseCLI (..))
-import Options.Applicative (Parser, command, hsubparser, info, progDesc)
+import HWM.Domain.ConfigT (ConfigT)
+import Options.Applicative (command, hsubparser, info, progDesc)
 import Relude
 
 -- | Top-level parser for 'release' command
-data ReleaseCommand = ReleasePackage Package.ReleasePackageOptions deriving (Show)
+data ReleaseCommand 
+    = ReleasePackage ReleasePackageOptions deriving (Show)
 
-parseCLI :: Parser ReleaseCommand
-parseCLI =
-  hsubparser
-    ( command "package" (info (ReleasePackage <$> parseCLI) (progDesc "Build, archive, hash, and export a package release"))
-    )
+instance ParseCLI ReleaseCommand where
+  parseCLI =
+    hsubparser
+      ( command "package" (info (ReleasePackage <$> parseCLI) (progDesc "Build, archive, hash, and export a package release"))
+      )
 
 runRelease :: ReleaseCommand -> ConfigT ()
 runRelease (ReleasePackage opts) = runReleasePackage opts
