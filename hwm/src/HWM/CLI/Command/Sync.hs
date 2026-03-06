8c06213053994a632d8a440b6af0f4fb93d49cd4
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-03-06 20:12:19 +0100

    hie.yaml

--- hwm/src/HWM/CLI/Command/Sync.hs
+++ hwm/src/HWM/CLI/Command/Sync.hs
@@ -26,10 +26,9 @@ sync tag = do
       ("resolver", pure $ buildResolver env)
     ]
   sectionConfig
-    ( [("stack.yaml", syncStackYaml $> chalk Green "✓") | buildStack env]
+    ( [("cabal.project", syncCabalProject $> chalk Green "✓")]
+        <> [("stack.yaml", syncStackYaml $> chalk Green "✓") | buildStack env]
         <> [("flake.nix", syncNixFile $> chalk Green "✓") | buildNix env]
-        <> [ ("hie.yaml", syncHie $> chalk Green "✓"),
-             ("cabal.project", syncCabalProject $> chalk Green "✓")
-           ]
+        <> [("hie.yaml", syncHie $> chalk Green "✓")]
     )
   syncPackages
