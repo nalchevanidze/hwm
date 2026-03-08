af9aeab1428a7c7c4ed520776dfdd44d8d801d47
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-03-08 19:06:27 +0100

    separate  golden tests from linrary

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
       haskellOverlay = final: prev: {
             hwmWorkspacePackages = prev.haskell.packages.ghc96.extend (hfinal: hprev: {
               hwm = hfinal.callCabal2nix "hwm" ./hwm {};
+              hwm = hfinal.callCabal2nix "hwm" ./hwm-golden {};
             });
       };
     in
@@ -20,6 +21,7 @@
         in
         {
           hwm = pkgs.hwmWorkspacePackages.hwm;
+          hwm = pkgs.hwmWorkspacePackages.hwm;
           default = pkgs.hwmWorkspacePackages.hwm;
         });
       devShells = forAllSystems (system:
@@ -28,7 +30,7 @@
         in
         {
           default = pkgs.hwmWorkspacePackages.shellFor {
-            packages = p: [ p.hwm ];
+            packages = p: [ p.hwm p.hwm ];
             buildInputs = with pkgs.hwmWorkspacePackages; [
               cabal-install
               haskell-language-server
---
+++ hwm-golden/README.md
@@ -0,0 +1,9 @@
+# HWM: Haskell Workspace Manager
+
+**HWM is not a build tool.** It is the missing link that orchestrates the tools you already use.
+
+📚 **[Complete Documentation & Examples →](https://github.com/nalchevanidze/hwm)**
+
+## Origin
+
+HWM was born out of necessity to manage the [Morpheus GraphQL](https://github.com/morpheusgraphql/morpheus-graphql) ecosystem with 15+ packages across multiple GHC versions.
