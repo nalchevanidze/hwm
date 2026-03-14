6374888b70128d4557a9402dc9026fdb15b2cd74
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-03-14 22:56:13 +0100

    environments

--- hwm/src/HWM/Integrations/Toolchain/Stack.hs
+++ hwm/src/HWM/Integrations/Toolchain/Stack.hs
@@ -145,7 +145,7 @@ deriveEnviromentName path = slugify <$> T.stripPrefix "stack-" (toText (dropExte
 buildMatrix :: (MonadIO m, MonadError Issue m) => [Pkg] -> [(Name, Stack)] -> m Environments
 buildMatrix pkgs (defaultEnv : envs) = do
   environments <- sortOn (profileGhc . snd) <$> traverse (inferBuildEnv pkgs) (defaultEnv : envs)
-  pure Environments {envsDefault = fst defaultEnv, envsProfiles = Map.fromList environments, envsStack = Just True, envsNix = Nothing, envsBuilder = Just StackBuilder}
+  pure Environments {envsDefault = fst defaultEnv, envsProfiles = Map.fromList environments, envsStack = Just True, envsNix = Nothing, envsBuilder = Just StackBuilder, envsHie = Nothing}
 buildMatrix _ [] = pure $ mkEnvironments latestGHCVersion
 
 inferBuildEnv :: (MonadIO m, MonadError Issue m) => [Pkg] -> (Name, Stack) -> m (Name, EnviromentProfile)
