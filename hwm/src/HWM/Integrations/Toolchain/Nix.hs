1781c483ac7248c0f7572b082d2f5293aad048e3
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-03-15 23:04:12 +0100

    rendergOverlayItem

--- hwm/src/HWM/Integrations/Toolchain/Nix.hs
+++ hwm/src/HWM/Integrations/Toolchain/Nix.hs
@@ -71,31 +71,26 @@ deriveFlakeNix releasePkgs ctx@(projectName, benv) ctxs =
 
 -- OVERLAY
 genOverlay :: Context -> [Context] -> [Text]
-genOverlay static benvs =
-  ["haskellOverlay = final: prev: {"]
-    <> concatMap rendergOverlayItem benvs
-    <> rendergOverlayStatic static
-    <> ["};"]
+genOverlay static benvs = fun "haskellOverlay = final: prev:" (concatMap rendergOverlayItem benvs <> rendergOverlayStatic static)
 
 rendergOverlayItem :: Context -> [Text]
 rendergOverlayItem ctx@(_, BuildEnvironment {..}) =
-  [ "  " <> genNixName ctx <> " = prev.haskell.packages." <> formatNixGhc buildGHC <> ".extend (hfinal: hprev: {"
-  ]
+  [genNixName ctx <> " = prev.haskell.packages." <> formatNixGhc buildGHC <> ".extend (hfinal: hprev: {"]
     <> map (renderPackageDef renderPackageBody) buildPkgs
-    <> ["  });"]
+    <> ["});"]
 
 rendergOverlayStatic :: Context -> [Text]
 rendergOverlayStatic (projectName, BuildEnvironment {..}) =
-  [ "  " <> genName (projectName, "static") <> " = prev.pkgsStatic.haskell.packages." <> formatNixGhc buildGHC <> ".extend (hfinal: hprev: {"
+  [ genName (projectName, "static") <> " = prev.pkgsStatic.haskell.packages." <> formatNixGhc buildGHC <> ".extend (hfinal: hprev: {"
   ]
     <> map (renderPackageDef (stripExecutables . renderPackageBody)) buildPkgs
-    <> ["  });"]
+    <> ["});"]
 
 stripExecutables :: (Semigroup a, IsString a) => a -> a
 stripExecutables x = "prev.haskell.lib.justStaticExecutables (" <> x <> ")"
 
 renderPackageDef :: (Pkg -> Text) -> Pkg -> Text
-renderPackageDef body pkg = "    " <> format (pkgName pkg) <> " =  " <> body pkg <> ";"
+renderPackageDef body pkg = "  " <> format (pkgName pkg) <> " =  " <> body pkg <> ";"
 
 renderPackageBody :: Pkg -> Text
 renderPackageBody pkg = " hfinal.callCabal2nix \"" <> format (pkgName pkg) <> "\" ./" <> format (pkgDirPath pkg) <> " {}"
